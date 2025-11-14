b388881bf150210d6556bf189bee0d30c363d545
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-11-14 12:22:51 +0530

    Added Helloworld

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-PERSON1="Trump"
-PERSON2="Musk"
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Hey $PERSON2, How are you doing?"
 echo "$PERSON2:: I'm doing great, thank you! How about you?"
