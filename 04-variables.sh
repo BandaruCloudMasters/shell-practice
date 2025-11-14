4e3a02f2920647d9ad2868a8c18315fd0f17d059
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-11-14 12:17:16 +0530

    Added Helloworld

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+PERSON1="Trump"
+PERSON2="Musk"
+
+echo "$PERSON1:: Hey $PERSON2, How are you doing?"
+echo "$PERSON2:: I'm doing great, thank you! How about you?"
+echo "$PERSON1:: I'm doing fantastic! Just won another election."
+echo "$PERSON2:: Congratulations! What are your plans for the future?"
+echo "$PERSON1:: I'm thinking about building a wall on Mars. What do you think?"
+echo "$PERSON2:: That sounds ambitious! Maybe we can collaborate on a space project."
+echo "$PERSON1:: Absolutely! Let's make space great again!"
